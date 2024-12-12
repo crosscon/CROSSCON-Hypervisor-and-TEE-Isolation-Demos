f483cb03a768f05fb24b77a2bfe4ddb941d806a9
Wiktor Grzywacz <wiktor.grzywacz@3mdeb.com>
2024-12-12 14:55:23 +0100

    remove tarballs, change debian version, update path

--- env/files/env.sh
+++ env/files/env.sh
@@ -1,4 +1,4 @@
-export PATH="./device-tree-compiler:$PATH"
-export PATH="./gcc-arm-11.2-2022.02-x86_64-aarch64-none-elf/bin:$PATH"
+export PATH="/work/device-tree-compiler:$PATH"
+export PATH="/work/gcc-arm-11.2-2022.02-x86_64-aarch64-none-elf/bin:$PATH"
 
 alias gcc="gcc-9"
