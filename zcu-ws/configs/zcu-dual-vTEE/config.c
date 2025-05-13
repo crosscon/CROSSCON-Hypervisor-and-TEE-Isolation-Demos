a9ec5d54d565c0765f185b306b498d0efdeb2106
David Cerdeira <davidmcerdeira@gmail.com>
2025-05-13 09:59:20 +0100

    Update ZCU104 configs

--- zcu-ws/configs/zcu-dual-vTEE/config.c
+++ zcu-ws/configs/zcu-dual-vTEE/config.c
@@ -1,6 +1,6 @@
 #include <config.h>
 
-VM_IMAGE(optee2_os_image, "../optee_os/optee2-aarch64/core/tee-pager_v2.bin");
+VM_IMAGE(optee2_os_image, "../optee_os/optee2-aarch64/core/tee-pager_v2.bin")
 
 struct vm_config optee2_os = {
     .image = {
@@ -17,7 +17,7 @@ struct vm_config optee2_os = {
     .platform = {
         .cpu_num = 1,
         .region_num = 1,
-        .regions = (struct mem_region[]) {
+        .regions = (struct vm_mem_region[]) {
             {
                 .base = 0x62000000,
                 .size = 0x00F00000, // 15 MB
@@ -32,7 +32,7 @@ struct vm_config optee2_os = {
             }
         },
         .dev_num = 1,
-        .devs = (struct dev_region[]) {
+        .devs = (struct vm_dev_region[]) {
             {
                 .pa = 0xFF000000,
                 .va = 0xFF000000,
@@ -49,9 +49,9 @@ struct vm_config optee2_os = {
 };
 
 // Linux Image
-VM_IMAGE(linux_image, "../lloader/linux-zcu.bin");
+VM_IMAGE(linux_image, "../lloader/linux-zcu.bin")
 // Linux VM configuration
-struct vm_config linux = {
+struct vm_config linux_vm = {
     .image = {
         .base_addr = 0x00200000,
         .load_addr = VM_IMAGE_OFFSET(linux_image),
@@ -67,7 +67,7 @@ struct vm_config linux = {
     .platform = {
         .cpu_num = 1,
         .region_num = 1,
-        .regions =  (struct mem_region[]) {
+        .regions =  (struct vm_mem_region[]) {
             {
                 .base = 0x00000000,
                 .size = 0x20000000
@@ -88,7 +88,7 @@ struct vm_config linux = {
 
         },
         .dev_num = 2,
-        .devs =  (struct dev_region[]) {
+        .devs =  (struct vm_dev_region[]) {
             {
                 /* UART1 */
                 .pa = 0xFF010000,
@@ -115,7 +115,7 @@ struct vm_config linux = {
     }
 };
 
-VM_IMAGE(optee_os_image, "../optee_os/optee-aarch64/core/tee-pager_v2.bin");
+VM_IMAGE(optee_os_image, "../optee_os/optee-aarch64/core/tee-pager_v2.bin")
 struct vm_config optee_os = {
     .image = {
         .base_addr = 0x60000000,
@@ -126,7 +126,7 @@ struct vm_config optee_os = {
     .cpu_affinity = 0xf,
 
     .children_num = 1,
-    .children = (struct vm_config*[]) { &linux },
+    .children = (struct vm_config*[]) { &linux_vm },
 
     .type = 1,
 
@@ -134,7 +134,7 @@ struct vm_config optee_os = {
     .platform = {
         .cpu_num = 1,
         .region_num = 1,
-        .regions = (struct mem_region[]) {
+        .regions = (struct vm_mem_region[]) {
             {
                 .base = 0x60000000,
                 .size = 0x00F00000, // 15 MB
@@ -149,7 +149,7 @@ struct vm_config optee_os = {
             }
         },
         .dev_num = 1,
-        .devs = (struct dev_region[]) {
+        .devs = (struct vm_dev_region[]) {
             {
                 .pa = 0xFF000000,
                 .va = 0xFF000000,
@@ -175,7 +175,7 @@ struct config config = {
         [1] = { .size = 0x00200000, },
     },
     .vmlist_size = 1,
-    .vmlist = {
+    .vmlist = (struct vm_config*[]) {
         &optee_os
     }
 };
--- zcu-ws/configs/zcu-single-vTEE/config.c
+++ zcu-ws/configs/zcu-single-vTEE/config.c
@@ -1,10 +1,10 @@
 #include <config.h>
 
 // Linux Image
-VM_IMAGE(linux_image, "../lloader/linux-zcu.bin");
+VM_IMAGE(linux_image, "../lloader/linux-zcu.bin")
 
 // Linux VM configuration
-struct vm_config linux = {
+struct vm_config linux_vm = {
     .image = {
         .base_addr = 0x00200000,
         .load_addr = VM_IMAGE_OFFSET(linux_image),
@@ -17,7 +17,7 @@ struct vm_config linux = {
     .platform = {
         .cpu_num = 1,
         .region_num = 1,
-        .regions =  (struct mem_region[]) {
+        .regions =  (struct vm_mem_region[]) {
             {
                 .base = 0x00000000,
                 .size = 0x20000000,
@@ -32,7 +32,7 @@ struct vm_config linux = {
             },
         },
         .dev_num = 4,
-        .devs =  (struct dev_region[]) {
+        .devs =  (struct vm_dev_region[]) {
             {
                 .pa   = 0x80000000,
                 .va   = 0x80000000,
@@ -77,7 +77,7 @@ struct vm_config linux = {
     }
 };
 
-VM_IMAGE(optee_os_image, "../optee_os/optee-aarch64/core/tee-pager_v2.bin");
+VM_IMAGE(optee_os_image, "../optee_os/optee-aarch64/core/tee-pager_v2.bin")
 struct vm_config optee_os = {
     .image = {
         .base_addr = 0x60000000,
@@ -88,7 +88,7 @@ struct vm_config optee_os = {
     .cpu_affinity = 0xf,
 
     .children_num = 1,
-    .children = (struct vm_config*[]) { &linux },
+    .children = (struct vm_config*[]) { &linux_vm },
 
     .type = 1,
 
@@ -96,7 +96,7 @@ struct vm_config optee_os = {
     .platform = {
         .cpu_num = 1,
         .region_num = 1,
-        .regions = (struct mem_region[]) {
+        .regions = (struct vm_mem_region[]) {
             {
                 .base = 0x60000000,
                 .size = 0x00F00000, // 15 MB
@@ -111,7 +111,7 @@ struct vm_config optee_os = {
             }
         },
         .dev_num = 1,
-        .devs = (struct dev_region[]) {
+        .devs = (struct vm_dev_region[]) {
             {
                 .va = 0xFF000000,
                 .pa = 0xFF000000,
@@ -136,7 +136,7 @@ struct config config = {
         [0] = { .size = 0x00200000, },
     },
     .vmlist_size = 1,
-    .vmlist = {
+    .vmlist = (struct vm_config*[]) {
         &optee_os
     }
 };
